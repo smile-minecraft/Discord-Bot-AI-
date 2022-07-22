7a830ad5d4b6c8189307e3c9f75c6ac0c96f22b5
smile-minecraft <54743433+smile-minecraft@users.noreply.github.com>
2022-07-22 12:56:31 +0800

    修復list的bug

--- commands/music/list.js
+++ commands/music/list.js
@@ -34,7 +34,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor(color.lightgreen)
             .setTitle('🎵 | 播放清單')
-            .setDescription(`${tracks.join('\n')}${
+            .setDescription(`${track === null ? '待播清單沒有東西' : tracks.join('\n')}${
                 queue.tracks.length > pageEnd
                     ? `\n...還有${queue.tracks.length - pageEnd} 首歌曲`
                     : ''
